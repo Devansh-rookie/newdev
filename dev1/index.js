98d39f907713b50e18492895a40e94061af5298e
devansh <devansh151005@gmail.com>
2024-08-10 17:25:36 +0530

    save

---
+++ dev1/index.js
@@ -0,0 +1,17 @@
+// Just put what and where
+ReactDOM.render(<h1>Hello Everyone!
+    <ul>
+        <li>
+        <ol>
+            <li>One</li>
+            <li>Two</li>
+        </ol>
+        </li>
+        <li>
+        <ol>
+            <li>I</li>
+            <li>II</li>
+        </ol>
+        </li>
+    </ul>
+</h1>, document.getElementById("hello"));
