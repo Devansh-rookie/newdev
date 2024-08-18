39cf9333cefcc070e1d69c4fbe2daed74e9a353a
Devansh-rookie <devansh151005@gmail.com>
2024-08-18 18:16:46 +0530

    dont know why is it not rendering

---
+++ something/src/components1/learn2.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Navbar from "./navbar";
+import Main from "./main";
+
+function Content(){
+    return (
+        <>
+        <Navbar/>
+        <Main/>
+        </>
+
+    );
+}
+
+// document.createElement('div', id = 'val');
+ReactDOM.render(<Content/>, document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Content/>);
